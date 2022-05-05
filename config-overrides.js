a5ac1c842c41927effa087da4d9a82efb3cb3caf
nc-minh <nguyencongminh.official@gmail.com>
2022-05-05 17:42:17 +0700

    :file_folder: update: constructure

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+/* config-overrides.js */
+
+const { override, useBabelRc } = require("customize-cra");
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+module.exports = override(useBabelRc());
