c8a91c04f4b27acca3a30cac4d5d1da98294f1e4
nc-minh <nguyencongminh.official@gmail.com>
2022-05-21 08:13:26 +0700

    :lipstick: setup global style

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,6 @@
+import './GlobalStyles.scss';
+function GlobalStyles({ children }) {
+  return children;
+}
+
+export default GlobalStyles;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GolobalStyles from '~/components/GlobalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GolobalStyles>
+      <App />
+    </GolobalStyles>
   </React.StrictMode>
 );
 
