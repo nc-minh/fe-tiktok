fdcc27418816fc1d10ac6b39e275f42e708fab81
Nguyen Cong Minh <59333368+nc-minh@users.noreply.github.com>
2022-09-02 00:27:37 +0700

    #5 update mobile version (#9)

--- src/app/Mobiles/components/MediaItem/components/Media/index.tsx
+++ src/app/Mobiles/components/MediaItem/components/Media/index.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import { isImage, isVideo } from 'utils/constants';
 import classNames from 'classnames/bind';
+import { useCallback, useRef, useState } from 'react';
 
 import styles from './Media.module.scss';
 import Image from 'app/components/Image';
+import PlayIcon from 'app/Mobiles/assets/icons/play.png';
 
 const cx = classNames.bind(styles);
 
@@ -11,15 +13,49 @@ interface Props {
   media?: any;
 }
 function Media(props: Props) {
+  const ref = useRef<any>(null);
   const { t } = useTranslation();
   const { media } = props;
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = useCallback(() => {
+    console.log('handlePlay on');
+    if (ref.current) {
+      ref.current.play();
+    }
+    setIsPlaying(true);
+  }, [isPlaying, setIsPlaying]);
+
+  const handleStop = useCallback(() => {
+    console.log('handlePlay off');
+    if (ref.current && isPlaying) {
+      ref.current.pause();
+    }
+
+    if (ref.current && !isPlaying) {
+      ref.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  }, [isPlaying, setIsPlaying]);
 
   if (isVideo(media?.media_url)) {
     return (
       <div className={cx('wrapper')}>
-        <video className={cx('media')} src={media?.media_url}>
+        <video
+          className={cx('media')}
+          src={media?.media_url}
+          ref={ref}
+          onClick={handleStop}
+          loop
+        >
           {t('text.notsupport')}
         </video>
+        {!isPlaying && (
+          <div className={cx('playWrapper')} onClick={handlePlay}>
+            <Image className={cx('play')} src={PlayIcon} />
+          </div>
+        )}
       </div>
     );
   }
